99d6d2b360c4fd881705ae95e4a0d2b09ad312a4
Matt <mattbriggs@yahoo.com>
2014-11-22 01:35:39 -0500

    email verification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,11 @@ RouteGuides::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :test
+  host = "routeguides.co.nz"
+  config.action_mailer.default_url_options = { host: host }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -77,4 +81,12 @@ RouteGuides::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+   :address => "madpom",
+   :port => 25,
+   :domain => "wharncliffe.co.nz",
+   :authentication => :none
+}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116174341) do
+ActiveRecord::Schema.define(version: 20141122024805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -341,6 +341,9 @@ ActiveRecord::Schema.define(version: 20141116174341) do
     t.datetime "lastVisited"
     t.string   "firstName"
     t.string   "lastName"
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
