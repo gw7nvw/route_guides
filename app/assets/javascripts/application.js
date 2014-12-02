38657b9af44bdc3a4ec99e75ab5594ec5c1c1ce8
Matt <mattbriggs@yahoo.com>
2014-12-02 17:11:14 -0500

    map resizing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui.min
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap.min
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-slider
